1fe51e19f92cff0ec7f6d44a37160db4003bb307
Saoki11111 <t.aoki11111@gmail.com>
2020-09-25 23:03:56 +0900

    init

---
+++ lett01.rb
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {Integer}
+def get_decimal_value(head)
+  sum = 0
+
+  while head != null
+    sum *= 2
+    sum += head.val
+    head = head.next
+  end
+
+  return sum
+    
+end
